7e15c3c2e87d4452c2d7583b09bcde4f14508003
ShivakumarReliant <shivakumar@reliantvision.com>
2024-02-21 19:30:32 +0530

    jahdsa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+const api = axios.create({
+  baseURL: 'http://localhost:5000', // Update with your Flask backend URL
+});
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    api.get('/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,3 +32,4 @@ function App() {
 }
 
 export default App;
+
