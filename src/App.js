8fd0e4b5011b8c9b5e747e31ae474dc03a329bb8
ShivakumarKasha <shivakumarkasha12@gmail.com>
2024-02-21 19:59:05 +0530

    dhaskj

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to InQUIZitive.
+          Welcome to InQUIZitive. today is wednesday
         </p>
       </header>
     </div>
